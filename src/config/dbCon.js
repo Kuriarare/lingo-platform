197a2b5d87387e105f113925f62ae7fa0fc93597
Carlos <139581721+Kuriarare@users.noreply.github.com>
2024-08-07 17:21:49 -0500

    Update dbCon.js

--- src/config/dbCon.js
+++ src/config/dbCon.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 
 const dbCon = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Increase timeout
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.error("DB Connection Error:", error);
